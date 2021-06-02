42617a76738f0c4663bfb1d604c59f4ee26c85bf
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-06-02 14:23:27 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -15,5 +15,18 @@ console.log('ml5 version',ml5.version);
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/g6otHPQcT/',modelLoaded);
 function modelLoaded(){
     console.log('model is loaded');
-    
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResults);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementsById("result_object_name").innerHTML=results[0].label;
+        document.getElementsById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
